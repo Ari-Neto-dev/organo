888df6af77f3edacd10d090150466b57334e9510
Ari Azevedo <azevedo.consultor@hotmail.com>
2022-09-14 11:56:53 -0300

    atualizando

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import'./Banner.css'
+
+function Banner(){
+  //JSX é como o react trabalha com a aparte visual
+  
+  return(
+    <header className="banner">
+    <img src="/imagens/banner.png" alt=""/>
+  
+   </header>
+  )
+
+
+}
+export default Banner
